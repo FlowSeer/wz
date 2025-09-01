24b8f5e428811f984948bf266ceb1c631179238d
Alexander tegtmeier <alexandertegtmeier@proton.me>
2025-09-01 11:46:36 +0200

    add ToXString for number types

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/FlowSeer/wz
 
 go 1.24.5
+
+require golang.org/x/exp v0.0.0-20250819193227-8b4c13bb791b // indirect
--- slices/string.go
+++ slices/string.go
@@ -1,6 +1,11 @@
 package slices
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+
+	"golang.org/x/exp/constraints"
+)
 
 // ToString returns a new slice of strings by calling the String method
 // of each element in the input slice s. The input slice must contain
@@ -20,6 +25,42 @@ func ToString[S ~[]E, E fmt.Stringer](s S) []string {
 
 	for i, v := range s {
 		res[i] = v.String()
+	}
+
+	return res
+}
+
+// ToIntString returns a new slice of strings by converting each integer element
+// in the input slice s to its string representation using strconv.Itoa.
+// The input slice must contain elements of an integer type.
+//
+// Example:
+//
+//	ints := []int{1, 2, 3}
+//	strs := slices.ToIntString(ints) // []string{"1", "2", "3"}
+func ToIntString[S ~[]E, E constraints.Integer](s S) []string {
+	res := make([]string, len(s))
+
+	for i, v := range s {
+		res[i] = strconv.Itoa(int(v))
+	}
+
+	return res
+}
+
+// ToFloatString returns a new slice of strings by converting each float element
+// in the input slice s to its string representation using strconv.FormatFloat.
+// The input slice must contain elements of a float type.
+//
+// Example:
+//
+//	floats := []float64{1.23, 4.56}
+//	strs := slices.ToFloatString(floats) // []string{"1.23", "4.56"}
+func ToFloatString[S ~[]E, E constraints.Float](s S) []string {
+	res := make([]string, len(s))
+
+	for i, v := range s {
+		res[i] = strconv.FormatFloat(float64(v), 'f', -1, 64)
 	}
 
 	return res
